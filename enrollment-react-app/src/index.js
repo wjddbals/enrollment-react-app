7b9f6d51aa1cd1a38d10f4a8689002087cc81268
FULLSTACK <jumin0220@naver.com>
2023-03-02 16:13:46 +0900

    React App 클릭이벤트 테스트

---
+++ enrollment-react-app/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/';
+import App from './App.js';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+      <h1>Just React</h1>
+  </React.StrictMode>,
+    document.querySelector('#app')
+);
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <h1>Just React</h1>
+        <App />
+    </React.StrictMode>,
+    document.querySelector('#root')
+);
+
