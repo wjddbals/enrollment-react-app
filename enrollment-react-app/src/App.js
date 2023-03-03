778a38a4d52a264681ec44c56cd866d19ffa72cf
FULLSTACK <jumin0220@naver.com>
2023-03-03 11:36:40 +0900

    props를 이용한 참가가능인원수 표시2학사석사구분

--- enrollment-react-app/src/App.js
+++ enrollment-react-app/src/App.js
@@ -5,29 +5,38 @@ import EnrollmentForm from "./components/EnrollmentForm";
 //정의된 앱 컴퍼넌트
 const App=()=>{
     const [program,setProgram]=useState("US");//프로그램 종류
-    const [seats,setSeats]=useState(100);//참가가능 인원수
+   // const [seats,setSeats]=useState(100);//참가가능 인원수
+    const [ugseats,setUgSeats]=useState(60);
+    const [pgseats,setPgSeats]=useState(40);
+
     const handleChange = (e) => {
         setProgram(e.target.value);
     };
 
-    //참가가능 인원수를 변경하는 함수
+    //참가가능 인원수를 변경하는 함수/
     const setUpdateSeats=(modifiySeat)=>{
-        setSeats(modifiySeat)
+        if(program ==='UG')
+            setUgSeats(modifiySeat);
+        else
+            setPgSeats(modifiySeat);
     };
     return (
         <div className="App">
             <div className="programs">
-                <label>프로그램 참가가능 인원수:{seats}</label>
+                <label>학사과정프로그램 참가가능 인원수:{ugseats}</label>
                 <br/>
+                <label>석사프로그램 참가가능 인원수:{pgseats}</label>
+                <br/><br/>
                 <label>프로그램 종류:</label>
                 <select className="appDropDowns"
                 onChange={handleChange} value={program}>
                     <option value="UG">학사과정(대학생)</option>
                     <option value="PG">석사과정(대학원생)</option>
                 </select>
+                <br/><br/><br/>
             </div>
             <EnrollmentForm chosenProgram={program}
-            currentSeat={seats}
+            currentSeat={(program==='UG') ? ugseats :pgseats}
                             setUpdateSeats={setUpdateSeats}/>
         </div>
 
