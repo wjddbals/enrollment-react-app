494d6438863bf4ec4366f1e236816e84f623d1fe
FULLSTACK <jumin0220@naver.com>
2023-03-09 17:31:16 +0900

    Rest -app-board list 구현-getInitialProps

--- semiprojectv2b-next-app-main/pages/api/board/list2.js
+++ semiprojectv2b-next-app-main/pages/api/board/list2.js
@@ -2,20 +2,20 @@ import fetch from 'isomorphic-unfetch';
 import maradb from 'mariadb';
 
 
-export default async (req,res){
+export default async (req,res)=>{
     let conn;
     const sql=`select bno,title,userid,date-formay(regdate,"%Y-%m-%d) regdate,` +
         `views from board order by bno desc`;
 try {
     conn =await maradb.createConnection(dbconfig);
     const rowData =await conn.query(sql);
-    res.status(200).json(err);
+    res.status(200).json(rowData);
 
 }catch (err){
-
+    res.status(500).json(err);
 
 }finally {
-    if (conn) await conn.query()
+    if (conn) await conn.close()
 
 }
 
