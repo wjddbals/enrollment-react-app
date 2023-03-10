03b9f4c83bb309694be6eaba0f0a1f15e4535db1
FULLSTACK <jumin0220@naver.com>
2023-03-10 10:00:43 +0900

    마리아 디비용 board 클래스  구현1-sql수정

---
+++ semiprojectv2b-next-app-main/pages/models/mariadb.js
@@ -0,0 +1,29 @@
+import mariadb from "mariadb";
+
+const mariadb = require('mariadb');
+const dbconfig = {
+    host: process.env.MARIADB_HOST,
+    user: process.env.MARIADB_USER,
+    password: process.env.MARIADB_PWD,
+    database: process.env.MARIADB_DB
+}
+
+const MariaDB = {
+
+
+
+    makeConn: async () => {
+        try {
+            return await maradb.createConnection(dbconfig);
+        } catch (e) { console.log(e); }
+    },
+
+    closeConn: async (conn) => {
+        if (conn) {
+            try { await conn.close(); }
+            catch (e) { console.log(e); }
+        }
+    }
+}
+
+module.exports = MariaDB;
