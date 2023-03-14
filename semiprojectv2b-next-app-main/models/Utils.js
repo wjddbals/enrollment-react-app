d09430da528c065a999e17f8b178a9739ff7a47f
FULLSTACK <jumin0220@naver.com>
2023-03-14 15:15:34 +0900

    마리아디비 board클래스 구현 4-회원비밀번호 암호화

--- semiprojectv2b-next-app-main/models/Utils.js
+++ semiprojectv2b-next-app-main/models/Utils.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import bcrypt from "bcryptjs";
 
 const check_captcha = async (response) => {
     let url = '/api/board/recaptcha?response=' + response;
@@ -12,6 +13,8 @@ const handleInput = (setInput, e) => {
     setInput(e.target.value);
 };
 
+
+
 const process_submit = async (url, data) => {
     const cnt = fetch(url, {
         method: 'POST', mode: 'cors',
@@ -22,4 +25,31 @@ const process_submit = async (url, data) => {
     return (await cnt).cnt;
 };
 
-module.exports = { check_captcha, handleInput, process_submit };
+
+const hashPassword =async (passwd)=>{
+    let saltRounds=10;
+
+    try {
+        const salt = await bcrypt.genSalt(saltRounds);
+        const hash = await bcrypt.hash(passwd,salt);
+        console.log('hashpwd - ',hash,passwd,salt);
+
+        return hash;
+    }catch (err){
+        console.log(err);
+    }
+}
+
+//암호 비교 함수 암호와 해시화된 암호를 비고
+const comparePasswd = async (passwd,hashpwd)=>{
+    try {
+        const result = await bcrypt.compare(passwd,hashpwd);
+        return result;
+    }catch (err){
+        console.log(err);
+    }
+
+}
+
+module.exports = { check_captcha, handleInput, process_submit,hashPassword,comparePasswd };
+
