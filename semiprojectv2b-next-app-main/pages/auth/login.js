5c4c42f4150b9c897506e646725ef5a37ab5fa1f
FULLSTACK <jumin0220@naver.com>
2023-03-14 16:09:35 +0900

    마리아디비 board클래스 구현5-NextAuth 1

---
+++ semiprojectv2b-next-app-main/pages/auth/login.js
@@ -0,0 +1,28 @@
+
+
+//경로 : /pages/auth/login.js
+
+import {signIn, signin, signOut, useSession} from "next-auth/client";
+
+const Login =()=>{
+    const [session,loading] =useSession();
+
+    if(session){
+        return (
+            <>
+                <h3>{session.user.email}계정으로 로그인하였습니다</h3>
+                <button onClick={()=> signOut()}>로그아웃하기</button>
+            </>
+        );
+
+    }//세션이 존재하면
+    return(
+        <>
+            <h3>로그인하세요</h3>
+            <button onClick={()=> signIn()}>로그인하기</button>
+        </>
+
+    );
+}
+
+export default Login;
